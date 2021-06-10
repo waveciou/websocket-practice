054ef26b8e30ccf0729eeddbab06ba6dc2eed10d
丘惟夫 <waveciou@gmail.com>
2021-06-10 21:50:36 +0800

    practice

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when client connects
+io.on('connection', socket => {
+  console.log('Hello World');
+
+  // Welcome to current user
+  socket.emit('message', 'Welcome to ChatCord');
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Run when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    io.emit('message', msg);
+  })
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
